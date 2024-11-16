17e23702d2821aa84e9e93ca859c449780015cad
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-16 21:00:17 +0300

    Написан get-запрос

--- src/main/java/todolist/controller/TodoController.java
+++ src/main/java/todolist/controller/TodoController.java
@@ -1,6 +1,10 @@
 package todolist.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todolist.entity.Todo;
@@ -20,7 +24,9 @@ public class TodoController {
 
 
     @GetMapping
-    public List<Todo> getTodoList(){
-        return todoRepository.findAll();
+    public ResponseEntity<List<Todo>> getToDos(@RequestHeader HttpHeaders headers) {
+        return new ResponseEntity<>
+                (this.todoRepository.findAll(),
+                        headers, HttpStatus.OK);
     }
 }
