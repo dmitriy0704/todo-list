d849fcd282fc87d9ae59e100400f244218ca3fad
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-16 22:45:17 +0300

    Написаны post, put и get{id} запросы

--- src/main/java/todolist/controller/TodoController.java
+++ src/main/java/todolist/controller/TodoController.java
@@ -3,14 +3,12 @@ package todolist.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import todolist.entity.Todo;
 import todolist.repository.TodoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -22,11 +20,34 @@ public class TodoController {
         this.todoRepository = todoRepository;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> todoById(@PathVariable("id") Long id) {
+        Optional<Todo> optional = todoRepository.findById(id);
+        return optional.map(todo ->
+                new ResponseEntity<>(todo, HttpStatus.OK)).orElseGet(() ->
+                new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 
     @GetMapping
     public ResponseEntity<List<Todo>> getToDos(@RequestHeader HttpHeaders headers) {
         return new ResponseEntity<>
                 (this.todoRepository.findAll(),
                         headers, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(
+            @PathVariable("id") Long id,
+            @RequestBody Todo todo
+    ){
+        todo.setId(id);
+        return todoRepository.save(todo);
     }
 }
