37af530e35f4a7bc065b632d5c134c1f28631f07
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-19 16:57:38 +0300

    Подключен контейнер с базой

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="dmitriy"
-
-ENTRYPOINT ["top", "-b"]
---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into roles (name)
+values
+    ('ROLE_USER'), ('ROLE_ADMIN');
+
+insert into users (username, password, email)
+values
+    ('user', '$2a$10$tSGpDUPfQoizhiyyp5TPB.KkvOzS6Sc90JlVe9d815cjRymJPpiA2', 'user@gmail.com'),
+    ('admin', '$2a$10$tSGpDUPfQoizhiyyp5TPB.KkvOzS6Sc90JlVe9d815cjRymJPpiA2', 'admin@gmail.com');
+
+insert into users_roles (user_id, role_id)
+values
+    (1, 1),
+    (2, 2);
